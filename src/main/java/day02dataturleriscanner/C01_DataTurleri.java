1d96174c19e30fc2ddecf37ad00942a27c6a35a9
busra idil <diyetisyenbusraidil@gmail.com>
2023-06-12 17:26:05 +0300

    day02C01

---
+++ src/main/java/day02dataturleriscanner/C01_DataTurleri.java
@@ -0,0 +1,51 @@
+package day02dataturleriscanner;
+
+public class C01_DataTurleri {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2= false;
+        //boolean b3= "Hello World";
+        //boolean b4=5;
+
+        //char c1= true;
+        //char c2="H";
+        char c3='v';
+        char c4='3';
+        char c5='*';
+        char c6=' ';
+        //char c7='m ';
+
+        String str1 = "v";
+        String str2= "5";
+
+        int sayi=5;//32 bit hafiza
+        short sayi2=5;//16 bit hafiza
+        byte sayi3= 5;//8 bit hafiza
+        long sayi4=5;//64 bit hafiza
+
+        float and1=2.384939563869F;
+        double and2= 3.34307504395046;
+        System.out.println(and1);//2.3849397 float virgulden sonra 6 hane gosterir
+        System.out.println(and2);//3.34307504395046   double virgulden sonra 16 hane gosterir
+
+        double d1 =20;
+        double d2 =5;
+        System.out.println( d1/d2 );//4.0 ==> 3.9999999999999999 veya 4.0000000000000001 yazabilir
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
