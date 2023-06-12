0b7c2fc3ae4f6fa9a992eedd02372131bb560cf8
busra idil <diyetisyenbusraidil@gmail.com>
2023-06-12 17:44:56 +0300

    day02C031

---
+++ src/main/java/day02dataturleriscanner/C02_NonPrimitiveDataTurleri.java
@@ -0,0 +1,25 @@
+package day02dataturleriscanner;
+
+public class C02_NonPrimitiveDataTurleri {
+    public static void main(String[] args) {
+        int sayi=10;
+        String str= "Java candir";
+
+        System.out.println(sayi);//10
+        System.out.println(str.toLowerCase());//java candir
+        System.out.println(str.toUpperCase());//JAVA CANDIR
+
+        /*
+        1-primitive data turleri sadece deger saklamanin yaninda hazir methodlar da barindirir
+        Bu hazir methodlar sayesinde biz tanimlanmis islemleri kolayca yapabiliriz
+
+        2-primitive data turlerinin isimleri kucuk harf ile baslar
+        non-primitive data turlerinin isimleri buyuk harf ile baslar
+
+
+
+
+         */
+
+    }
+}
---
+++ src/main/java/day02dataturleriscanner/C03_NamingConvention.java
@@ -0,0 +1,7 @@
+package day02dataturleriscanner;
+
+public class C03_NamingConvention {
+    public static void main(String[] args) {
+
+    }
+}
