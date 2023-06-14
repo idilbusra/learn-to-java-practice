1c10086b4f3cb2bade3ede245cc5630bf2064517
busra idil <diyetisyenbusraidil@gmail.com>
2023-06-14 11:42:08 +0300

    day03C06

---
+++ src/main/java/day03_dataCasting_Incement/C06_IslemOnceligi.java
@@ -0,0 +1,33 @@
+package day03_dataCasting_Incement;
+
+public class C06_IslemOnceligi {
+    public static void main(String[] args) {
+        System.out.println(8+5+2*(12/4)+8);//27
+        System.out.println(35/5);//7
+        System.out.println(20/8);//2.5 ==>2
+        //bolunen iki sayi da int olursa sonucu int olarak yazdirir
+
+        double dbl1= 20;
+        System.out.println(dbl1/8);//2.5
+
+        int sayi1=20;
+        int sayi2=8;
+
+        //sayi1 / sayi2'yi ondalikli sayi olarak yazdirin
+
+        System.out.println(  (double) sayi1/sayi2    );//2.5
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
