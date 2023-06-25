8479212e5a31b257ea7c3814993bbf1cc0218321
busra idil <diyetisyenbusraidil@gmail.com>
2023-06-25 23:05:13 +0300

    day10C07V2

--- src/main/java/day10_nestedForLoop_methodOlusturma/C07_MethodOlusturma.java
+++ src/main/java/day10_nestedForLoop_methodOlusturma/C07_MethodOlusturma.java
@@ -11,5 +11,17 @@ public class C07_MethodOlusturma {
         ikiSayiTopla(67, 87);
 
     }
-}
+        public static void ikiSayiTopla(int sayi1, int sayi2){
+        /*
+            public : access modifier ==> erisim belirleyici, simdilik hep public yapalim
+            static : ozel bir keyword'dur ileride anlatilacak, simdilik hep static yapalim
+            void   : return type , method'un bana bir sey dondurmeyecegini, sadece yazdiracagini belirtir
+            isim   : method'un yaptigi is ile baglantili olmali
+            (parametreler) : method'un yapacagi is icin bizim gondermemiz gereken degiskenler
+         */
+
+            System.out.println("Iki sayinin toplami : " + (sayi1+ sayi2));
+        }
+
+    }
 
