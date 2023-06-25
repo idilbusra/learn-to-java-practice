3922842604e8af693d3f8e4bf44c9efb5b80ab2a
busra idil <diyetisyenbusraidil@gmail.com>
2023-06-25 23:03:54 +0300

    day10C07

---
+++ src/main/java/day10_nestedForLoop_methodOlusturma/C07_MethodOlusturma.java
@@ -0,0 +1,15 @@
+package day10_nestedForLoop_methodOlusturma;
+
+public class C07_MethodOlusturma {
+    public static void main(String[] args) {
+
+        // verilen iki sayinin toplamini yazdiran bir method olusturun
+
+
+        ikiSayiTopla(5, 9);
+        ikiSayiTopla(7, 1);
+        ikiSayiTopla(67, 87);
+
+    }
+}
+
